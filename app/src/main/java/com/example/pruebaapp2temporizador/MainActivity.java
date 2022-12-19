ad7bab107a0cdd17c4824cb936ade63188e034de
ssanchez195 <aguanozo2001@gmail.com>
2022-12-19 21:27:09 +0100

    Cambio de nombre a la aplicacion

--- app/src/main/java/com/example/pruebaapp2temporizador/MainActivity.java
+++ app/src/main/java/com/example/pruebaapp2temporizador/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Screen View
         TimerTask timerTask= new TimerTask() {
             @Override
             public void run() {
